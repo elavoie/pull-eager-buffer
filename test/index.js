953513035c2db9b7f23b7573aa2ddb22347b6c36
Erick <github@ericklavoie.com>
2016-12-08 11:51:00 -0500

    Added a lot more tests to test various interleavings

--- test/index.js
+++ test/index.js
@@ -116,5 +116,193 @@ tape('draining while read is delayed', function (t) {
   t.deepEqual(actual, expected)
 })
 
-// TODO: Add more tests for testing various interleavings of
-//       close events from both ends, with and without data inside
+tape('closing the buffer', function (t) {
+  var _cb, b, _abort
+
+  function expRead (expAbort) {
+    t.equal(_abort, expAbort)
+  }
+  function sink (abort, x) {
+    _cb(abort, x)
+  }
+
+  function expNext (expErr, expX) {
+    return function next (err, x) {
+      if (err && err !== true) t.fail('unexpected error:' + err)
+      t.equal(err, expErr)
+      if (!err) {
+        t.equal(x, expX)
+      }
+    }
+  }
+
+  function drain (abort, expNext) {
+    b.source(abort, expNext)
+  }
+
+  function setup () {
+    b = buffer()
+    b.sink(function read (abort, cb) {
+      _abort = abort
+      _cb = cb
+    })
+  }
+
+  setup()
+  expRead(false)
+  sink(true)
+  drain(null, expNext(true))
+
+  setup()
+  expRead(false)
+  drain(null, expNext(true))
+  sink(true)
+
+  setup()
+  expRead(false)
+  drain(true, expNext(true))
+  sink(true)
+
+  setup()
+  expRead(false)
+  drain(true, expNext(true))
+  sink(null, 1)
+  expRead(true)
+  drain(null, expNext(true))
+  sink(null, 1)
+  expRead(true)
+  sink(null, 1)
+  expRead(true)
+
+  t.end()
+})
+
+tape('Testing various interleavings', function (t) {
+  var _cb, b, _abort
+
+  function expRead (expAbort) {
+    t.equal(_abort, expAbort)
+    _abort = undefined
+  }
+  function sink (abort, x) {
+    _cb(abort, x)
+  }
+
+  var waiting = []
+  function expNext (expErr, expX) {
+    var next = function (err, x) {
+      if (waiting.length < 1 || waiting[0] !== next) {
+        t.fail('Invalid callback order resolution')
+      } else {
+        waiting.shift()
+      }
+
+      if (err && err !== true) t.fail('unexpected error:' + err)
+      t.equal(err, expErr)
+      if (!err) {
+        t.equal(x, expX)
+      }
+    }
+
+    waiting.push(next)
+    return next
+  }
+
+  function drain (abort, expNext) {
+    b.source(abort, expNext)
+  }
+
+  function setup () {
+    b = buffer()
+    b.sink(function read (abort, cb) {
+      if (_abort !== undefined) {
+        // Test that the previous value was false
+        expRead(false)
+      }
+      _abort = abort
+      _cb = cb
+    })
+  }
+
+  function end () {
+    if (waiting.length > 0) {
+      t.fail('Unresolved callbacks')
+    } else {
+      t.end()
+    }
+  }
+
+  setup()
+  sink(true)
+  drain(null, expNext(true))
+  drain(null, expNext(true))
+
+  setup()
+  drain(true, expNext(true))
+  sink(null, 1)
+  expRead(true)
+
+  setup()
+  expRead(false)
+  drain(true, expNext(true))
+  sink(true)
+  // read should not have been called
+  // so the value should be undefined
+  expRead(undefined)
+
+  setup()
+  drain(null, expNext(null, 1))
+  sink(null, 1)
+  expRead(false)
+  sink(true)
+  drain(null, expNext(true))
+  expRead(undefined)
+
+  setup()
+  sink(null, 1)
+  drain(null, expNext(null, 1))
+  drain(null, expNext(true))
+  expRead(false)
+  sink(true)
+  expRead(undefined)
+
+  setup()
+  sink(null, 1)
+  drain(null, expNext(null, 1))
+  sink(true)
+  drain(null, expNext(true))
+  drain(null, expNext(true))
+
+  setup()
+  sink(null, 1)
+  sink(null, 2)
+  sink(null, 3)
+  drain(null, expNext(null, 1))
+  sink(null, 4)
+  sink(true)
+  drain(null, expNext(null, 2))
+  drain(null, expNext(null, 3))
+  drain(null, expNext(null, 4))
+  drain(null, expNext(true))
+
+  setup()
+  sink(null, 1)
+  sink(null, 2)
+  sink(null, 3)
+  drain(null, expNext(null, 1))
+  sink(null, 4)
+  drain(null, expNext(null, 2))
+  drain(null, expNext(null, 3))
+  drain(null, expNext(null, 4))
+  expRead(false)
+  drain(null, expNext(true))
+  // Drain should actually be waiting
+  // for the sink to provide more values
+  // or end the stream
+  t.equal(waiting.length === 1, true)
+
+  // Now it is properly done
+  sink(true)
+
+  end()
+})
